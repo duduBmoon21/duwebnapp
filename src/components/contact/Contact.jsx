05eb86766077a16d797c02576bb6b022ce8e0013
Eleanor <140634996+duduBmoon21@users.noreply.github.com>
2024-04-03 20:17:08 +0300

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
             >
               <h1 className="text-5xl font-bold sm:text-3xl">You Need</h1>
               <h3 className="text-xl sm:text-lg">
-                Beautiful design for your website leave a request
+                 Web or Mobile App leave a request
               </h3>
             </div>
           </div>
